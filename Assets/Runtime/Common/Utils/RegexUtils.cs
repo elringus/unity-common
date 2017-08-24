ca6edb756eaa4a53091ea9b8f799453eb372e4f1
Artyom Sovetnikov <elringus@yandex.ru>
2017-08-25 01:33:34 +0300

    add regex utils

---
+++ Assets/Runtime/Common/Utils/RegexUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+public static class RegexUtils
+{
+    /// <summary>
+    /// Get index of the last character in the match.
+    /// </summary>
+    public static int GetEndIndex (this Match match)
+    {
+        return match.Index + match.Length - 1;
+    }
+}
