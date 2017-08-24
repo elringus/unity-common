27138670aed0973851944581fd41d40b819b0d5d
Artyom Sovetnikov <elringus@yandex.ru>
2017-08-25 01:33:27 +0300

    add linq utils

---
+++ Assets/Runtime/Common/Utils/LinqUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class LinqUtils
+{
+    /// <summary>
+    /// Removes last item in the list.
+    /// </summary>
+    public static void RemoveLastItem<T> (this List<T> list, Predicate<T> predicate = null)
+    {
+        if (list == null || list.Count == 0) return;
+
+        var elementIndex = predicate == null ? list.Count - 1 : list.FindIndex(predicate);
+        if (elementIndex >= 0)
+            list.RemoveAt(elementIndex);
+    }
+}
