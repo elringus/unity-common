e63531f6850dc15e5c0e6c0db9ac65b1730ad95b
Artyom Sovetnikov <elringus@yandex.ru>
2017-09-23 21:55:10 +0300

    fix texture size

--- Assets/UnityCommon/Editor/EditorUtils.cs
+++ Assets/UnityCommon/Editor/EditorUtils.cs
@@ -40,6 +40,7 @@ public static class EditorUtils
     {
         var wrapMode = texture.wrapMode;
         var alphaIsTransparency = texture.alphaIsTransparency;
+        var maxSize = Mathf.Max(texture.width, texture.height);
 
         path = string.Format("{0}/{1}.png", path.GetBefore("/", false), texture.name);
         Debug.Assert(AssetDatabase.IsValidFolder(path.GetBefore("/")));
@@ -55,6 +56,7 @@ public static class EditorUtils
         textureImporter.wrapMode = wrapMode;
         textureImporter.mipmapEnabled = generateMipmaps;
         textureImporter.textureCompression = compression;
+        textureImporter.maxTextureSize = maxSize;
         AssetDatabase.ImportAsset(path);
 
         if (destroyInitialTextureObject)
