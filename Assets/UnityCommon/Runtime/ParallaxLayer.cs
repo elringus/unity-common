ebce55df9e8b0b43307d8cbc37e39b570ac3e95d
Artyom Sovetnikov <elringus@yandex.ru>
2017-09-29 14:10:05 +0300

    update editor

---
+++ Assets/UnityCommon/Runtime/ParallaxLayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    public Camera Camera { get { return _camera; } }
+    public float ParallaxFactor { get { return _parallaxFactor; } }
+
+    private Transform cameraTrs;
+    private float initialOffset;
+
+    [SerializeField] private Camera _camera = null;
+    [Range(0f, 1f)]
+    [SerializeField] private float _parallaxFactor = 1f;
+
+    private void Awake ()
+    {
+        cameraTrs = Camera ? Camera.transform : Camera.main.transform;
+        Debug.Assert(cameraTrs, "Assign required objects to ParallaxLayer.");
+        initialOffset = (transform.position.x - cameraTrs.position.x) / ParallaxFactor;
+    }
+
+    private void Update ()
+    {
+        var targetPosX = cameraTrs.position.x + initialOffset;
+        transform.SetPosX(targetPosX * ParallaxFactor);
+    }
+}
--- Assets/UnityCommon/Runtime/ScriptableUI/ScriptableSlider.cs
+++ Assets/UnityCommon/Runtime/ScriptableUI/ScriptableSlider.cs
@@ -1,28 +1,34 @@
-﻿// Copyright 2014-2017 Artyom Sovetnikov. All Rights Reserved.
+﻿// Copyright 2012-2017 Elringus (Artyom Sovetnikov). All Rights Reserved.
 
 namespace UnityCommon
 {
-// Copyright 2014-2017 Artyom Sovetnikov. All Rights Reserved.
-
-namespace UnityCommon
-{
-using UnityEngine.UI;
-
-public abstract class ScriptableSlider : ScriptableUIControl<Slider>
-{
-    protected override void BindUIEvents ()
+    // Copyright 2014-2017 Artyom Sovetnikov. All Rights Reserved.
+    
+    namespace UnityCommon
     {
-        UIComponent.onValueChanged.AddListener(OnValueChanged);
-    }
-
-    protected override void UnbindUIEvents ()
+    // Copyright 2014-2017 Artyom Sovetnikov. All Rights Reserved.
+    
+    namespace UnityCommon
     {
-        UIComponent.onValueChanged.RemoveListener(OnValueChanged);
+    using UnityEngine.UI;
+    
+    public abstract class ScriptableSlider : ScriptableUIControl<Slider>
+    {
+        protected override void BindUIEvents ()
+        {
+            UIComponent.onValueChanged.AddListener(OnValueChanged);
+        }
+    
+        protected override void UnbindUIEvents ()
+        {
+            UIComponent.onValueChanged.RemoveListener(OnValueChanged);
+        }
+    
+        protected abstract void OnValueChanged (float value);
     }
-
-    protected abstract void OnValueChanged (float value);
-}
-
-}
-
+    
+    }
+    
+    }
+    
 }
