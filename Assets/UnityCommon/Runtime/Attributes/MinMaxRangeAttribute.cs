1ebb81f8b9cd551854b35c9168919dbea0240b66
Artyom Sovetnikov <elringus@yandex.ru>
2017-09-30 22:44:11 +0300

    add minmaxrange

---
+++ Assets/UnityCommon/Runtime/Attributes/MinMaxRangeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MinMaxRangeAttribute : PropertyAttribute
+{
+    public readonly float Min;
+    public readonly float Max;
+
+    public MinMaxRangeAttribute (float min, float max)
+    {
+        Min = min;
+        Max = max;
+    }
+}
