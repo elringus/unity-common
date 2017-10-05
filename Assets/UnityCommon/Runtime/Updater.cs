4fcf9ce68382512e906599a878d20852c4840a81
Artyom Sovetnikov <elringus@yandex.ru>
2017-10-05 12:58:19 +0300

    add updater

---
+++ Assets/UnityCommon/Runtime/Updater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Updater : MonoBehaviour
+{
+    private UnityAction[] actions = new UnityAction[0];
+
+    private void Update ()
+    {
+        var length = actions.Length;
+        for (int i = 0; i < length; i++)
+            actions[i].Invoke();
+    }
+
+    private void OnApplicationQuit ()
+    {
+        actions = null;
+    }
+
+    public void AddAction (UnityAction action)
+    {
+        Array.Resize(ref actions, actions.Length + 1);
+        actions[actions.Length - 1] = action;
+    }
+}
