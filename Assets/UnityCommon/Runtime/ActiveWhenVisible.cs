94dd2e97ffa5b30d92f248ec8d60a565c19ba88c
Artyom Sovetnikov <elringus@yandex.ru>
2017-10-07 13:32:14 +0300

    add active when visible

---
+++ Assets/UnityCommon/Runtime/ActiveWhenVisible.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ActiveWhenVisible : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour[] affectedScripts;
+
+    private void Start ()
+    {
+        SetEnabled(false);
+    }
+
+    private void OnBecameVisible ()
+    {
+        SetEnabled(true);
+    }
+
+    private void OnBecameInvisible ()
+    {
+        SetEnabled(false);
+    }
+
+    private void SetEnabled (bool isEnabled)
+    {
+        if (affectedScripts == null) return;
+
+        var length = affectedScripts.Length;
+        for (int i = 0; i < length; i++)
+            affectedScripts[i].enabled = isEnabled;
+    }
+}
