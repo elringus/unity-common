aaf49b7cd292e8c431e9d0570bb4748782dc12a5
Artyom Sovetnikov <elringus@yandex.ru>
2017-10-09 19:40:05 +0300

    add color utils

---
+++ Assets/UnityCommon/Runtime/Utils/ColorUtils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class ColorUtils
+{
+    /// <summary>
+    /// Solid white color with zero alpha.
+    /// </summary>
+    public static Color ClearWhite { get { return clearWhite; } }
+
+    private static Color clearWhite = new Color(1, 1, 1, 0);
+}
