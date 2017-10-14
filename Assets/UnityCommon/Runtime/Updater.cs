972946142c9e1a15e21d7b72e2d0adc94aa2f174
Artyom Sovetnikov <elringus@yandex.ru>
2017-10-14 16:36:18 +0300

    add update delay

--- Assets/UnityCommon/Runtime/Updater.cs
+++ Assets/UnityCommon/Runtime/Updater.cs
@@ -4,13 +4,23 @@ using UnityEngine.Events;
 
 public class Updater : MonoBehaviour
 {
+    public float UpdateDelay { get { return _updateDelay; } set { _updateDelay = value; } }
+
+    [SerializeField] private float _updateDelay = 0f;
+
     private UnityAction[] actions = new UnityAction[0];
+    private float lastUpdateTime = 0f;
 
     private void Update ()
     {
+        var timeSinceLastUpdate = Time.time - lastUpdateTime;
+        if (timeSinceLastUpdate < UpdateDelay) return;
+
         var length = actions.Length;
         for (int i = 0; i < length; i++)
             actions[i].Invoke();
+
+        lastUpdateTime = Time.time;
     }
 
     private void OnDestroy ()
