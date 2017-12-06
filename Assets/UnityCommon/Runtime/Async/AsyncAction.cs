806e7d7a6bd324e24c5f14941fad516d996e18b7
Artyom Sovetnikov <elringus@yandex.ru>
2017-12-06 19:26:19 +0300

    refactor async

--- Assets/UnityCommon/Runtime/Async/AsyncAction.cs
+++ Assets/UnityCommon/Runtime/Async/AsyncAction.cs
@@ -7,18 +7,39 @@ using UnityEngine;
 public class AsyncAction : CustomYieldInstruction
 {
     /// <summary>
-    /// Event invoked when asynchronous operation has completed execution.
+    /// Event invoked when action has completed execution.
     /// </summary>
     public event Action OnCompleted;
 
+    /// <summary>
+    /// Whether action has completed execution.
+    /// </summary>
     public virtual bool IsCompleted { get; protected set; }
+    /// <summary>
+    /// Whether action can instantly complete execution 
+    /// and use <see cref="CompleteInstantly"/>.
+    /// </summary>
     public virtual bool CanBeInstantlyCompleted { get { return true; } }
     public override bool keepWaiting { get { return !IsCompleted; } }
 
+    /// <summary>
+    /// Forces the action to complete instantly.
+    /// Works only when <see cref="CanBeInstantlyCompleted"/>.
+    /// </summary>
     public virtual void CompleteInstantly ()
     {
         if (!CanBeInstantlyCompleted || IsCompleted) return;
         HandleOnCompleted();
+    }
+
+    /// <summary>
+    /// Adds a delegate to invoke when the action is completed execution.
+    /// If the action is already completed, the delegate will be invoked immediately.
+    /// </summary>
+    public virtual void Then (Action action)
+    {
+        if (IsCompleted) action.SafeInvoke();
+        else OnCompleted += action;
     }
 
     protected virtual void HandleOnCompleted ()
@@ -33,11 +54,39 @@ public class AsyncAction : CustomYieldInstruction
 /// </summary>
 public class AsyncAction<TResult> : AsyncAction 
 {
+    /// <summary>
+    /// Event invoked when action has completed execution.
+    /// </summary>
+    public new event Action<TResult> OnCompleted;
+
+    /// <summary>
+    /// Result of the action execution.
+    /// </summary>
     public TResult Result { get; protected set; }
 
+    /// <summary>
+    /// Forces action to complete instantly by providing the <see cref="Result"/> object.
+    /// Works only when <see cref="CanBeInstantlyCompleted"/>.
+    /// </summary>
     public virtual void CompleteInstantly (TResult result)
     {
         Result = result;
         base.CompleteInstantly();
+    }
+
+    /// <summary>
+    /// Adds a delegate to invoke when the action is completed execution.
+    /// If the action is already completed, the delegate will be invoked immediately.
+    /// </summary>
+    public virtual void Then (Action<TResult> action)
+    {
+        if (IsCompleted) action.SafeInvoke(Result);
+        else OnCompleted += action;
+    }
+
+    protected override void HandleOnCompleted ()
+    {
+        base.HandleOnCompleted();
+        OnCompleted.SafeInvoke(Result);
     }
 }
