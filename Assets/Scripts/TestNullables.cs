976ba1c11062eff5676023928b502cab04b9b0b5
Artyom Sovetnikov <elringus@yandex.ru>
2017-12-31 21:15:22 +0300

    implement nullables

---
+++ Assets/Scripts/TestNullables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNullables : MonoBehaviour
+{
+    public NullableFloat Float;
+    public NullableString String;
+    public NullableBool Bool;
+    public NullableVector3 Vector3;
+    public List<NullableVector3> Vector3List;
+
+    private IEnumerator Start ()
+    {
+        yield return new WaitForSeconds(2);
+
+        Vector3List[2].Value = UnityEngine.Vector3.one;
+    }
+}
