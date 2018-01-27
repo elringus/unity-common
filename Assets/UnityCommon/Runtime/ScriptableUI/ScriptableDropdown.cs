2298b53b8d1b019e0286bb613591a31cd3bb3f8a
Artyom Sovetnikov <elringus@yandex.ru>
2018-01-27 12:36:44 +0300

    add scriptable dropdown

---
+++ Assets/UnityCommon/Runtime/ScriptableUI/ScriptableDropdown.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UI;
+
+public abstract class ScriptableDropdown : ScriptableUIControl<Dropdown>
+{
+    protected override void BindUIEvents ()
+    {
+        UIComponent.onValueChanged.AddListener(OnValueChanged);
+    }
+
+    protected override void UnbindUIEvents ()
+    {
+        UIComponent.onValueChanged.RemoveListener(OnValueChanged);
+    }
+
+    protected abstract void OnValueChanged (int value);
+}
