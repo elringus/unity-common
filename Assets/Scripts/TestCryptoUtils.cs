62fa06374fa8fc78d4704000a8fd9873882510e8
Artyom Sovetnikov <elringus@yandex.ru>
2018-01-31 17:42:19 +0300

    implement crypto utils

---
+++ Assets/Scripts/TestCryptoUtils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TestCryptoUtils : MonoBehaviour
+{
+    public string Input;
+
+    private void OnEnable ()
+    {
+        print("CryptoUtils.PersistentHashCode: " + CryptoUtils.PersistentHashCode(Input));
+        print("CryptoUtils.PersistentHexCode: " + CryptoUtils.PersistentHexCode(Input));
+    }
+}
