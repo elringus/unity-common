ad40e8a853ddbef9adab8427e4bf3f4d009d38cd
Artyom Sovetnikov <elringus@yandex.ru>
2018-03-06 19:07:49 +0300

    add loop

--- Assets/UnityCommon/Runtime/Timer.cs
+++ Assets/UnityCommon/Runtime/Timer.cs
@@ -3,22 +3,29 @@ using UnityEngine;
 
 public class Timer : AsyncRunner<object>
 {
+    public event Action OnLoop;
+
     public override bool CanBeInstantlyCompleted { get { return true; } }
+    public bool Loop { get; private set; }
     public bool IsTimeScaleIgnored { get; private set; }
     public float Duration { get; private set; }
     public float ElapsedTime { get; private set; }
 
-    public Timer (float duration = 0f, bool ignoreTimeScale = false, 
-        MonoBehaviour coroutineContainer = null, Action onCompleted = null) : base(coroutineContainer, onCompleted)
+    public Timer (float duration = 0f, bool loop = false, bool ignoreTimeScale = false, 
+        MonoBehaviour coroutineContainer = null, Action onCompleted = null, Action onLoop = null) : base(coroutineContainer, onCompleted)
     {
         Duration = duration;
+        Loop = loop;
         IsTimeScaleIgnored = ignoreTimeScale;
+
+        if (onLoop != null) OnLoop += onLoop;
     }
 
-    public Timer Run (float duration, bool ignoreTimeScale = false)
+    public Timer Run (float duration, bool loop = false, bool ignoreTimeScale = false)
     {
         ElapsedTime = 0f;
         Duration = duration;
+        Loop = loop;
         IsTimeScaleIgnored = ignoreTimeScale;
 
         base.Run();
@@ -28,7 +35,14 @@ public class Timer : AsyncRunner<object>
 
     public override AsyncRunner<object> Run ()
     {
-        return Run(Duration, IsTimeScaleIgnored);
+        return Run(Duration, Loop, IsTimeScaleIgnored);
+    }
+
+    public override void Stop ()
+    {
+        base.Stop();
+
+        Loop = false;
     }
 
     protected override bool LoopCondition ()
@@ -41,5 +55,16 @@ public class Timer : AsyncRunner<object>
         base.OnCoroutineTick();
 
         ElapsedTime += IsTimeScaleIgnored ? Time.unscaledDeltaTime : Time.deltaTime;
+    }
+
+    protected override void HandleOnCompleted ()
+    {
+        if (Loop)
+        {
+            OnLoop.SafeInvoke();
+            base.Stop();
+            Run();
+        }
+        else base.HandleOnCompleted();
     }
 }
