d5d7337e41c5157024c2c371ec8c769b62151573
Artyom Sovetnikov <elringus@yandex.ru>
2018-04-24 17:53:22 +0300

    fix unity 2018 build processing

--- Assets/UnityCommon/Runtime/ResourceProvider/ProjectResources.cs
+++ Assets/UnityCommon/Runtime/ResourceProvider/ProjectResources.cs
@@ -40,36 +40,53 @@ public class ProjectResources : ScriptableObject
 }
 
 #if UNITY_EDITOR
-public class ProjectResourcesPreprocessor : UnityEditor.Build.IPreprocessBuild, UnityEditor.Build.IPostprocessBuild
+public class ProjectResourcesPreprocessor :
+    #if UNITY_2018_1_OR_NEWER
+    UnityEditor.Build.IPreprocessBuildWithReport, UnityEditor.Build.IPostprocessBuildWithReport
+    #else
+    UnityEditor.Build.IPreprocessBuild, UnityEditor.Build.IPostprocessBuild
+    #endif
 {
     public int callbackOrder { get { return 0; } }
-
+    
     protected string DIR_PATH { get { return "Assets/Resources"; } }
     protected string ASSET_PATH { get { return DIR_PATH + "/ProjectResources.asset"; } }
-
+    
     private bool folderCreated;
-
+    
     public void OnPreprocessBuild (UnityEditor.BuildTarget target, string path)
     {
         var asset = ScriptableObject.CreateInstance<ProjectResources>();
         asset.LocateAllResourceFolders();
-
+    
         if (!System.IO.Directory.Exists(DIR_PATH))
         {
             System.IO.Directory.CreateDirectory(DIR_PATH);
             folderCreated = true;
         }
         else folderCreated = false;
-
+    
         UnityEditor.AssetDatabase.CreateAsset(asset, ASSET_PATH);
         UnityEditor.AssetDatabase.SaveAssets();
     }
-
+    
     public void OnPostprocessBuild (UnityEditor.BuildTarget target, string path)
     {
         UnityEditor.AssetDatabase.DeleteAsset(ASSET_PATH);
         if (folderCreated) UnityEditor.AssetDatabase.DeleteAsset(DIR_PATH);
         UnityEditor.AssetDatabase.SaveAssets();
     }
+
+    #if UNITY_2018_1_OR_NEWER
+    public void OnPreprocessBuild (UnityEditor.Build.Reporting.BuildReport report)
+    {
+        OnPreprocessBuild(report.summary.platform, report.summary.outputPath);
+    }
+
+    public void OnPostprocessBuild (UnityEditor.Build.Reporting.BuildReport report)
+    {
+        OnPostprocessBuild(report.summary.platform, report.summary.outputPath);
+    }
+    #endif
 }
 #endif
