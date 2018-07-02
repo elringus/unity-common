ab7ea367f9372e405f752067d034482ff0eaf179
Artyom Sovetnikov <elringus@yandex.ru>
2018-07-02 19:14:55 +0300

    add test script

---
+++ Assets/Editor/EditorTestScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorTestScript
+{
+    [MenuItem("Test/Install Shader")]
+    private static void InstallShader ()
+    {
+        BlendModes.ExtensionsManager.InstallShaderExtension("UIDefault");
+    }
+
+    [MenuItem("Test/Install Comp")]
+    private static void InstallComp ()
+    {
+        BlendModes.ExtensionsManager.InstallComponentExtension("UnityEngine.UI.Image");
+    }
+
+    [MenuItem("Test/Remove Shader")]
+    private static void RemoveShader ()
+    {
+        BlendModes.ExtensionsManager.RemoveShaderExtension("UIDefault");
+    }
+
+    [MenuItem("Test/Remove Comp")]
+    private static void RemoveComp ()
+    {
+        BlendModes.ExtensionsManager.RemoveComponentExtension("UnityEngine.UI.Image");
+    }
+}
