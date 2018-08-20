ae010669e3e39334e9528f22a9ee4e90c590841d
Artyom Sovetnikov <elringus@yandex.ru>
2018-08-20 19:47:49 +0300

    add labeled button

---
+++ Assets/UnityCommon/Runtime/ScriptableUI/ScriptableLabeledButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UnityCommon
+{
+    public class ScriptableLabeledButton : ScriptableUIControl<LabeledButton>
+    {
+        public event Action OnButtonClicked;
+
+        public override bool IsInteractable => CanvasGroup ? base.IsInteractable : UIComponent.interactable;
+
+        public override void SetIsInteractable (bool isInteractable)
+        {
+            if (CanvasGroup) base.SetIsInteractable(isInteractable);
+            else UIComponent.interactable = isInteractable;
+        }
+
+        protected override void BindUIEvents ()
+        {
+            UIComponent.onClick.AddListener(OnButtonClick);
+            UIComponent.onClick.AddListener(OnButtonClicked.SafeInvoke);
+        }
+
+        protected override void UnbindUIEvents ()
+        {
+            UIComponent.onClick.RemoveListener(OnButtonClick);
+            UIComponent.onClick.RemoveListener(OnButtonClicked.SafeInvoke);
+        }
+
+        protected virtual void OnButtonClick () { }
+    }
+}
