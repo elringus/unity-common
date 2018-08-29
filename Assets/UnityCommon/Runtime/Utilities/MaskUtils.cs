0180866afe64053158884eb26a207d9e2f162368
Artyom Sovetnikov <elringus@yandex.ru>
2018-08-29 13:45:45 +0300

    add non ref overloads

--- Assets/UnityCommon/Runtime/Utilities/MaskUtils.cs
+++ Assets/UnityCommon/Runtime/Utilities/MaskUtils.cs
@@ -10,6 +10,12 @@ namespace UnityCommon
             else mask = mask | ~(1 << layer);
         }
 
+        public static int SetLayer (int mask, int layer, bool enabled)
+        {
+            if (enabled) return mask | (1 << layer);
+            else return mask | ~(1 << layer);
+        }
+
         public static bool GetLayer (int mask, int layer)
         {
             return mask == (mask | (1 << layer));
@@ -19,6 +25,12 @@ namespace UnityCommon
         {
             var layer = LayerMask.NameToLayer(layerName);
             SetLayer(ref mask, layer, enabled);
+        }
+
+        public static int SetLayer (int mask, string layerName, bool enabled)
+        {
+            var layer = LayerMask.NameToLayer(layerName);
+            return SetLayer(mask, layer, enabled);
         }
 
         public static bool GetLayer (int mask, string layerName)
