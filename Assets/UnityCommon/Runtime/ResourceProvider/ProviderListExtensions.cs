b356a2c267e725e1822aefb86a5f4f568aa8a6c5
Artyom Sovetnikov <elringus@yandex.ru>
2018-09-20 18:37:56 +0300

    add resource loader

---
+++ Assets/UnityCommon/Runtime/ResourceProvider/ProviderListExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnityCommon
+{
+    public static class ProviderListExtensions
+    {
+        /// <summary>
+        /// Loads a resource at the provided path.
+        /// When resources with equal paths are available in multiple providers, will load the one from the higher-priority provider.
+        /// </summary>
+        public static async Task<Resource<T>> LoadResourceAsync<T> (this List<IResourceProvider> providers, string path)
+        {
+            var resource = default(Resource<T>);
+            if (providers.Count == 1)
+                resource = await providers[0].LoadResourceAsync<T>(path);
+            else
+            {
+                foreach (var provider in providers)
+                {
+                    if (!await provider.ResourceExistsAsync<T>(path)) continue;
+                    resource = await provider.LoadResourceAsync<T>(path);
+                    break;
+                }
+            }
+            return resource;
+        }
+
+        /// <summary>
+        /// Loads all the resources at the provided path from all the providers.
+        /// When a resource is available in multiple providers, will only load the one from the higher-priority provider.
+        /// </summary>
+        public static async Task<IEnumerable<Resource<T>>> LoadResourcesAsync<T> (this List<IResourceProvider> providers, string path)
+        {
+            var resources = new List<Resource<T>>();
+            if (providers.Count == 1)
+                resources.AddRange(await providers[0].LoadResourcesAsync<T>(path));
+            else
+            {
+                foreach (var provider in providers)
+                {
+                    var locatedResources = await provider.LocateResourcesAsync<T>(path);
+                    foreach (var locatedResource in locatedResources)
+                        if (!resources.Any(r => r.Path == locatedResource.Path))
+                            resources.Add(await provider.LoadResourceAsync<T>(locatedResource.Path));
+                }
+            }
+            return resources;
+        }
+
+        /// <summary>
+        /// Locates all the resources at the provided path from all the providers.
+        /// When a resource is available in multiple providers, will only get the one from the higher-priority provider.
+        /// </summary>
+        public static async Task<IEnumerable<Resource<T>>> LocateResourcesAsync<T> (this List<IResourceProvider> providers, string path)
+        {
+            var result = new List<Resource<T>>();
+            foreach (var provider in providers)
+            {
+                var locatedResources = await provider.LocateResourcesAsync<T>(path);
+                foreach (var locatedResource in locatedResources)
+                    if (!result.Any(r => r.Path == locatedResource.Path))
+                        result.Add(locatedResource);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Checks whether a resource at the provided path exists in any of the providers.
+        /// </summary>
+        public static async Task<bool> ResourceExistsAsync<T> (this List<IResourceProvider> providers, string path)
+        {
+            foreach (var provider in providers)
+                if (await provider.ResourceExistsAsync<T>(path)) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Unloads resource at the provided path from all the providers in the list.
+        /// </summary>
+        /// <param name="path">Path to the resource location.</param>
+        public static async Task UnloadResourceAsync (this List<IResourceProvider> providers, string path)
+        {
+            foreach (var provider in providers)
+                await provider.UnloadResourceAsync(path);
+        }
+
+        /// <summary>
+        /// Unloads all loaded resources from all the providers in the list.
+        /// </summary>
+        public static async Task UnloadResourcesAsync (this List<IResourceProvider> providers)
+        {
+            foreach (var provider in providers)
+                await provider.UnloadResourcesAsync();
+        }
+
+        /// <summary>
+        /// Checks whether resource with the provided path is loaded by any of the providers in the list.
+        /// </summary>
+        public static bool ResourceLoaded (this List<IResourceProvider> providers, string path)
+        {
+            foreach (var provider in providers)
+                if (provider.ResourceLoaded(path)) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Checks whether resource with the provided path is currently being loaded by any of the providers in the list.
+        /// </summary>
+        public static bool ResourceLoading (this List<IResourceProvider> providers, string path)
+        {
+            foreach (var provider in providers)
+                if (provider.ResourceLoading(path)) return true;
+            return false;
+        }
+    }
+}
---
+++ Assets/UnityCommon/Runtime/ResourceProvider/ResourceLoader.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnityCommon
+{
+    /// <summary>
+    /// Allows working with resources using a prioritized providers list.
+    /// </summary>
+    public abstract class ResourceLoader
+    {
+        public abstract bool IsLoadingAny { get; }
+
+        protected List<IResourceProvider> Providers { get; }
+        protected string Prefix { get; }
+
+        public ResourceLoader (List<IResourceProvider> providersList, string resourcePathPrefix = null)
+        {
+            Providers = providersList;
+            Prefix = resourcePathPrefix;
+        }
+
+        public abstract Task PreloadAsync (string path, bool isFullPath = false);
+        public abstract bool IsLoadedByProvider (string path, bool isFullPath = false);
+        public abstract bool IsLoaded (string path, bool isFullPath = false);
+        public abstract Task UnloadAsync (string path, bool isFullPath = false);
+        public abstract Task UnloadAllAsync ();
+    }
+
+    /// <summary>
+    /// Allows working with resources of specific type using a prioritized providers list.
+    /// </summary>
+    public class ResourceLoader<TResource> : ResourceLoader
+    {
+        public override bool IsLoadingAny => loadCounter > 0;
+        public Dictionary<string, TResource> LoadedResources { get; }
+
+        private int loadCounter;
+
+        public ResourceLoader (List<IResourceProvider> providersList, string resourcePathPrefix = null)
+            : base(providersList, resourcePathPrefix)
+        {
+            LoadedResources = new Dictionary<string, TResource>();
+        }
+
+        public virtual async Task<TResource> LoadAsync (string path, bool isFullPath = false)
+        {
+            IncrementLoadCounter();
+            if (!isFullPath) path = BuildFullPath(path);
+
+            var resource = await Providers.LoadResourceAsync<TResource>(path);
+
+            AddLoadedResource(resource);
+            DecrementLoadCounter();
+            return resource.Object;
+        }
+
+        public virtual async Task<IEnumerable<TResource>> LoadAllAsync (string path = null, bool isFullPath = false)
+        {
+            IncrementLoadCounter();
+            if (!isFullPath) path = BuildFullPath(path);
+
+            var resources = await Providers.LoadResourcesAsync<TResource>(path);
+
+            AddLoadedResources(resources);
+            DecrementLoadCounter();
+            return resources.Select(r => r.Object);
+        }
+
+        public virtual async Task<IEnumerable<Resource<TResource>>> LocateResourcesAsync (string path, bool isFullPath = false)
+        {
+            if (!isFullPath) path = BuildFullPath(path);
+            return await Providers.LocateResourcesAsync<TResource>(path);
+        }
+
+        public override async Task PreloadAsync (string path, bool isFullPath = false)
+        {
+            await LoadAsync(path, isFullPath);
+        }
+
+        public override async Task UnloadAsync (string path, bool isFullPath = false)
+        {
+            if (!isFullPath) path = BuildFullPath(path);
+
+            await Providers.UnloadResourceAsync(path);
+
+            RemoveLoadedResource(path);
+        }
+
+        public override async Task UnloadAllAsync ()
+        {
+            var resources = LoadedResources.Keys.ToArray();
+            for (int i = 0; i < 0; i++)
+                await UnloadAsync(resources[i], true);
+        }
+
+        /// <summary>
+        /// Whether a resource with the provided path is loaded by any of the available providers.
+        /// </summary>
+        public override bool IsLoadedByProvider (string path, bool isFullPath = false)
+        {
+            if (!isFullPath) path = BuildFullPath(path);
+            return Providers.ResourceLoaded(path);
+        }
+
+        /// <summary>
+        /// Whether a resource with the provided path is already loaded 
+        /// and can be instantly retrieved via <see cref="GetLoaded(string, bool)"/>.
+        /// </summary>
+        public override bool IsLoaded (string path, bool isFullPath = false)
+        {
+            if (!isFullPath) path = BuildFullPath(path);
+            return LoadedResources.ContainsKey(path);
+        }
+
+        /// <summary>
+        /// Returns a loaded resource with the provided path.
+        /// In case the resource is not loaded, will return null.
+        /// </summary>
+        public virtual TResource GetLoaded (string path, bool isFullPath = false)
+        {
+            if (!isFullPath) path = BuildFullPath(path);
+            if (!IsLoaded(path, true)) return default(TResource);
+            return LoadedResources[path];
+        }
+
+        protected virtual string BuildFullPath (string path)
+        {
+            if (!string.IsNullOrWhiteSpace(Prefix))
+            {
+                if (!string.IsNullOrWhiteSpace(path)) return $"{Prefix}/{path}";
+                else return Prefix;
+            }
+            else return path;
+        }
+
+        protected virtual void AddLoadedResources (IEnumerable<Resource<TResource>> resources)
+        {
+            foreach (var resource in resources)
+                AddLoadedResource(resource);
+        }
+
+        protected virtual void AddLoadedResource (Resource<TResource> resource)
+        {
+            LoadedResources[resource.Path] = resource.Object;
+        }
+
+        protected virtual void RemoveLoadedResource (string resourcePath)
+        {
+            if (LoadedResources.ContainsKey(resourcePath))
+                LoadedResources.Remove(resourcePath);
+        }
+
+        protected void IncrementLoadCounter () => loadCounter++;
+        protected void DecrementLoadCounter () => loadCounter--;
+    }
+}
