15fa60a736256d0310eb8948ab904a72fb28fa02
Artyom Sovetnikov <elringus@yandex.ru>
2018-10-09 19:05:01 +0300

    add int range

---
+++ Assets/UnityCommon/Runtime/Collections/IntRange.cs
@@ -0,0 +1,25 @@
+﻿
+namespace UnityCommon
+{
+    /// <summary>
+    /// Represents an integer range starting with <see cref="StartIndex"/> and ending with <see cref="EndIndex"/>.
+    /// Both endpoints are considered to be included.
+    /// </summary>
+    [System.Serializable]
+    public struct IntRange
+    {
+        public int StartIndex;
+        public int EndIndex;
+
+        public IntRange (int startIndex, int endIndex)
+        {
+            StartIndex = startIndex;
+            EndIndex = endIndex;
+        }
+
+        public bool Contains (int index)
+        {
+            return index >= StartIndex && index <= EndIndex;
+        }
+    }
+}
