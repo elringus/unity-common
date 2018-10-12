707db54210851f393fb8b5781cca7468ddc20374
Artyom Sovetnikov <elringus@yandex.ru>
2018-10-12 20:27:58 +0300

    reimplement map

--- Assets/UnityCommon/Runtime/Collections/LiteralMap.cs
+++ Assets/UnityCommon/Runtime/Collections/LiteralMap.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace UnityCommon
 {
     /// <summary>
     /// Dictionary with case-insensitive string keys.
     /// </summary>
-    public class LiteralMap<TValue> : Dictionary<string, TValue>
+    public class LiteralMap<TValue> : Map<string, TValue>
     {
         public LiteralMap () : base(StringComparer.OrdinalIgnoreCase) { }
     }
+
+    /// <summary>
+    /// A serializable version of <see cref="LiteralMap{TValue}"/> with string values.
+    /// </summary>
+    [Serializable]
+    public class SerializableLiteralStringMap : LiteralMap<string> { }
 }
