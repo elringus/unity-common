600f0695e8a3179b6c85d99f8cd5932cc680976a
Artyom Sovetnikov <elringus@yandex.ru>
2018-12-29 20:56:41 +0300

    add zoffset

--- Assets/UnityCommon/Runtime/Gameplay/Billboard.cs
+++ Assets/UnityCommon/Runtime/Gameplay/Billboard.cs
@@ -7,6 +7,11 @@ namespace UnityCommon
         private Transform cameraTransform;
         private Transform myTransform;
 
+        [Tooltip("Whether to apply a local z-offset to compensate root disposition with the parent transform.")]
+        [SerializeField] private bool applyZOffset = false;
+        [Tooltip("Used for the z-offset. Should be equal to the parent transform height, in units.")]
+        [SerializeField] private float parentHeight = 1f;
+
         private void Awake ()
         {
             cameraTransform = Camera.main.transform;
@@ -26,6 +31,12 @@ namespace UnityCommon
         private void LateUpdate ()
         {
             myTransform.forward = cameraTransform.forward;
+
+            if (transform.parent && applyZOffset)
+            {
+                var zOffset = Mathf.Tan(transform.rotation.eulerAngles.x * Mathf.Deg2Rad) * parentHeight / 2f;
+                transform.localPosition = new Vector3(0, 0, zOffset);
+            }
         }
 
         private void OnBecameVisible ()
