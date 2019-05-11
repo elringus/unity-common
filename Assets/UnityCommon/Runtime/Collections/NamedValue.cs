9a9c49bb4f89c66a222486d77b13be88a1f3407b
Artyom Sovetnikov <elringus@yandex.ru>
2019-05-11 14:45:20 +0300

    add named value

---
+++ Assets/UnityCommon/Runtime/Collections/NamedValue.cs
@@ -0,0 +1,17 @@
+﻿
+namespace UnityCommon
+{
+    /// <summary>
+    /// Represents a container for a generic named item.
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value</typeparam>
+    [System.Serializable]
+    public class NamedValue<TValue> : Pair<string, TValue>
+    {
+        public string Name { get => Item1; set => Item1 = value; }
+        public TValue Value { get => Item2; set => Item2 = value; }
+
+        public NamedValue (string name, TValue value)
+            : base(name, value) { }
+    }
+}
