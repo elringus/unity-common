92df4fd35d788b5a303efbf93d8909ee93a0775c
Artyom Sovetnikov <elringus@yandex.ru>
2019-11-28 19:38:51 +0300

    publish

---
+++ Runtime/Collections/SerializableNamed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace UnityCommon
+{
+    /// <summary>
+    /// Represents a serializable container for a generic named item.
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value</typeparam>
+    [System.Serializable]
+    public abstract class SerializableNamed<TValue> : Named<TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] string name = default;
+
+        public SerializableNamed (string name, TValue value)
+            : base(name, value) { }
+
+        public virtual void OnAfterDeserialize ()
+        {
+            Name = name;
+        }
+
+        public virtual void OnBeforeSerialize ()
+        {
+            name = Name;
+        }
+    }
+}
