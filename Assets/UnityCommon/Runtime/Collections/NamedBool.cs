4f442c121bff07dc88b30a99cb4f41e1c6844054
Artyom Sovetnikov <elringus@yandex.ru>
2019-11-28 19:38:26 +0300

    add serilizable named

---
+++ Assets/UnityCommon/Runtime/Collections/NamedBool.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace UnityCommon
+{
+    /// <summary>
+    /// Represents a serializable container for a bool named item.
+    /// </summary>
+    [System.Serializable]
+    public class NamedBool : SerializableNamed<bool>
+    {
+        [SerializeField] private bool value = default;
+
+        public NamedBool (string name, bool value)
+            : base(name, value) { }
+
+        public override void OnAfterDeserialize ()
+        {
+            base.OnAfterDeserialize();
+
+            Value = value;
+        }
+
+        public override void OnBeforeSerialize ()
+        {
+            base.OnBeforeSerialize();
+
+            value = Value;
+        }
+    }
+}
