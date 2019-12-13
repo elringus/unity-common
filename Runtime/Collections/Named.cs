bf8fbdd4fbd6be926693dabc635c80242559b3d5
Artyom Sovetnikov <elringus@yandex.ru>
2019-12-13 17:28:39 +0300

    publish

--- Runtime/Collections/Named.cs
+++ Runtime/Collections/Named.cs
@@ -1,17 +1,91 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace UnityCommon
 {
     /// <summary>
-    /// Represents a container for a generic named item.
+    /// Represents a container for a <see cref="string"/> (name) and a generic value 
+    /// with support for Unity serialization (for derived non-generic types).
     /// </summary>
-    /// <typeparam name="TValue">Type of the value</typeparam>
-    [System.Serializable]
-    public class Named<TValue> : Pair<string, TValue>
+    /// <typeparam name="TValue">Type of the value; should be natively supported by the Unity serialization system.</typeparam> 
+    [Serializable]
+    public class Named<TValue> : IEquatable<Named<TValue>>
     {
-        public string Name { get => Item1; set => Item1 = value; }
-        public TValue Value { get => Item2; set => Item2 = value; }
+        /// <summary>
+        /// Name of the item.
+        /// </summary>
+        public string Name { get => name; set => name = value; }
+        /// <summary>
+        /// Value of the item.
+        /// </summary>
+        public TValue Value { get => value; set => this.value = value; }
 
-        public Named (string name, TValue value)
-            : base(name, value) { }
+        [SerializeField] private string name = default;
+        [SerializeField] private TValue value = default;
+
+        public override bool Equals (object obj)
+        {
+            return Equals(obj as Named<TValue>);
+        }
+
+        public bool Equals (Named<TValue> other)
+        {
+            return other != null &&
+                   name == other.name &&
+                   EqualityComparer<TValue>.Default.Equals(value, other.value);
+        }
+
+        public override int GetHashCode ()
+        {
+            var hashCode = 1477024672;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<TValue>.Default.GetHashCode(value);
+            return hashCode;
+        }
+
+        public static bool operator == (Named<TValue> left, Named<TValue> right)
+        {
+            return EqualityComparer<Named<TValue>>.Default.Equals(left, right);
+        }
+
+        public static bool operator != (Named<TValue> left, Named<TValue> right)
+        {
+            return !(left == right);
+        }
     }
+
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableString"/> value.
+    /// </summary>
+    [Serializable] public class NamedString : Named<NullableString> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableInteger"/> value.
+    /// </summary>
+    [Serializable] public class NamedInteger : Named<NullableInteger> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableFloat"/> value.
+    /// </summary>
+    [Serializable] public class NamedFloat : Named<NullableFloat> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableBoolean"/> value.
+    /// </summary>
+    [Serializable] public class NamedBoolean : Named<NullableBoolean> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableVector3"/> value.
+    /// </summary>
+    [Serializable] public class NamedVector2 : Named<NullableVector3> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableVector3"/> value.
+    /// </summary>
+    [Serializable] public class NamedVector3 : Named<NullableVector3> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableVector4"/> value.
+    /// </summary>
+    [Serializable] public class NamedVector4 : Named<NullableVector4> { }
+    /// <summary>
+    /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableQuaternion"/> value.
+    /// </summary>
+    [Serializable] public class NamedQuaternion : Named<NullableQuaternion> { }
 }
+   
---
+++ Runtime/Collections/Nullable.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityCommon
+{
+    /// <summary>
+    /// Represents a <see cref="Nullable"/> with support for Unity serialization (for derived non-generic types).
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value; should be natively supported by the Unity serialization system.</typeparam>
+    [Serializable]
+    public class Nullable<TValue> : IEquatable<Nullable<TValue>>
+    {
+        /// <summary>
+        /// Current value when <see cref="HasValue"/>, default otherwise.
+        /// </summary>
+        public TValue Value { get => GetValue(); set => SetValue(value); }
+        /// <summary>
+        /// Whether the value is assigned.
+        /// </summary>
+        public bool HasValue { get => GetHasValue(); set => SetHasValue(value); }
+
+        /// <summary>
+        /// Whether <typeparamref name="TValue"/> is a value type.
+        /// </summary>
+        protected bool IsValueType => typeof(TValue).IsValueType;
+
+        [SerializeField] private TValue value = default;
+        [SerializeField] private bool hasValue = default;
+
+        public override bool Equals (object obj)
+        {
+            return Equals(obj as Nullable<TValue>);
+        }
+
+        public bool Equals (Nullable<TValue> other)
+        {
+            return other != null &&
+                   EqualityComparer<TValue>.Default.Equals(value, other.value) &&
+                   hasValue == other.hasValue;
+        }
+
+        public override int GetHashCode ()
+        {
+            var hashCode = 1753382938;
+            hashCode = hashCode * -1521134295 + EqualityComparer<TValue>.Default.GetHashCode(value);
+            hashCode = hashCode * -1521134295 + hasValue.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator == (Nullable<TValue> left, Nullable<TValue> right)
+        {
+            return EqualityComparer<Nullable<TValue>>.Default.Equals(left, right);
+        }
+
+        public static bool operator != (Nullable<TValue> left, Nullable<TValue> right)
+        {
+            return !(left == right);
+        }
+
+        protected virtual TValue GetValue ()
+        {
+            return HasValue ? value : default;
+        }
+
+        protected virtual void SetValue (TValue value)
+        {
+            this.value = value;
+
+            HasValue = IsValueType || EqualityComparer<TValue>.Default.Equals(value, default);
+        }
+
+        protected virtual bool GetHasValue ()
+        {
+            return hasValue;
+        }
+
+        protected virtual void SetHasValue (bool hasValue)
+        {
+            this.hasValue = hasValue;
+        }
+    }
+
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="string"/>.
+    /// </summary>
+    [Serializable] public class NullableString : Nullable<string> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="int"/>.
+    /// </summary>
+    [Serializable] public class NullableInteger : Nullable<int> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="float"/>.
+    /// </summary>
+    [Serializable] public class NullableFloat : Nullable<float> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="bool"/>.
+    /// </summary>
+    [Serializable] public class NullableBoolean : Nullable<bool> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="Vector2"/>.
+    /// </summary>
+    [Serializable] public class NullableVector2 : Nullable<Vector2> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="Vector3"/>.
+    /// </summary>
+    [Serializable] public class NullableVector3 : Nullable<Vector3> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="Vector4"/>.
+    /// </summary>
+    [Serializable] public class NullableVector4 : Nullable<Vector4> { }
+    /// <summary>
+    /// Represents a serializable <see cref="System.Nullable"/> <see cref="Quaternion"/>.
+    /// </summary>
+    [Serializable] public class NullableQuaternion : Nullable<Quaternion> { }
+}
