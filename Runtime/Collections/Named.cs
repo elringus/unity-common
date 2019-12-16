d9b4c7fd8a6311fa716ddfb06c7e18489acaacd3
Artyom Sovetnikov <elringus@yandex.ru>
2019-12-16 11:49:38 +0300

    publish

--- Runtime/Collections/Named.cs
+++ Runtime/Collections/Named.cs
@@ -24,6 +24,14 @@ namespace UnityCommon
         [SerializeField] private NullableString name = default;
         [SerializeField] private TValue value = default;
 
+        public Named () { }
+
+        public Named (string name, TValue value)
+        {
+            Name = name;
+            Value = value;
+        }
+
         public override bool Equals (object obj)
         {
             return Equals(obj as Named<TValue>);
@@ -58,34 +66,81 @@ namespace UnityCommon
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableString"/> value.
     /// </summary>
-    [Serializable] public class NamedString : Named<NullableString> { }
+    [Serializable]
+    public class NamedString : Named<NullableString>
+    {
+        public NamedString () { }
+        public NamedString (string name, string value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableInteger"/> value.
     /// </summary>
-    [Serializable] public class NamedInteger : Named<NullableInteger> { }
+    [Serializable]
+    public class NamedInteger : Named<NullableInteger>
+    {
+        public NamedInteger () { }
+        public NamedInteger (string name, int? value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableFloat"/> value.
     /// </summary>
-    [Serializable] public class NamedFloat : Named<NullableFloat> { }
+    [Serializable]
+    public class NamedFloat : Named<NullableFloat>
+    {
+        public NamedFloat () { }
+        public NamedFloat (string name, float? value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableBoolean"/> value.
     /// </summary>
-    [Serializable] public class NamedBoolean : Named<NullableBoolean> { }
+    [Serializable]
+    public class NamedBoolean : Named<NullableBoolean>
+    {
+        public NamedBoolean () { }
+        public NamedBoolean (string name, bool? value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableVector3"/> value.
     /// </summary>
-    [Serializable] public class NamedVector2 : Named<NullableVector3> { }
+    [Serializable]
+    public class NamedVector2 : Named<NullableVector2>
+    {
+        public NamedVector2 () { }
+        public NamedVector2 (string name, Vector2? value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableVector3"/> value.
     /// </summary>
-    [Serializable] public class NamedVector3 : Named<NullableVector3> { }
+    [Serializable]
+    public class NamedVector3 : Named<NullableVector3>
+    {
+        public NamedVector3 () { }
+        public NamedVector3 (string name, Vector3? value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableVector4"/> value.
     /// </summary>
-    [Serializable] public class NamedVector4 : Named<NullableVector4> { }
+    [Serializable]
+    public class NamedVector4 : Named<NullableVector4>
+    {
+        public NamedVector4 () { }
+        public NamedVector4 (string name, Vector4? value) : base(name, value) { }
+    }
+
     /// <summary>
     /// Represents a serializable <see cref="Named{TValue}"/> with <see cref="NullableQuaternion"/> value.
     /// </summary>
-    [Serializable] public class NamedQuaternion : Named<NullableQuaternion> { }
+    [Serializable]
+    public class NamedQuaternion : Named<NullableQuaternion>
+    {
+        public NamedQuaternion () { }
+        public NamedQuaternion (string name, Quaternion? value) : base(name, value) { }
+    }
 }
    
