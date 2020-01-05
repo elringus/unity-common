cdc34f767af929ee91c4e8c5662be50ce38b5b99
Artyom Sovetnikov <elringus@yandex.ru>
2020-01-05 14:29:46 +0300

    handle legacy input

--- Assets/UnityCommon/Runtime/Utilities/EventUtils.cs
+++ Assets/UnityCommon/Runtime/Utilities/EventUtils.cs
@@ -12,15 +12,19 @@ namespace UnityCommon
         /// </summary>
         public static GameObject GetHoveredGameObject (this EventSystem eventSystem)
         {
+            #if ENABLE_LEGACY_INPUT_MANAGER
             var pointerEventData = new PointerEventData(EventSystem.current);
             pointerEventData.position = Input.touchCount > 0 ? (Vector3)Input.GetTouch(0).position : Input.mousePosition;
 
             var raycastResults = new List<RaycastResult>();
             EventSystem.current.RaycastAll(pointerEventData, raycastResults);
-
             if (raycastResults.Count > 0)
                 return raycastResults[0].gameObject;
             else return null;
+            #else
+            Debug.LogWarning("`UnityCommon.GetHoveredGameObject` requires legacy input system, which is disabled; the method will always return null.");
+            return null;
+            #endif
         }
 
         public static void SafeInvoke (this Action action)
