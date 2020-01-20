2871b7f8d93a357702fa15bef7ef4e0f5ca81ad8
Artyom Sovetnikov <elringus@yandex.ru>
2020-01-20 14:44:03 +0300

    expose temp path

--- Assets/UnityCommon/Editor/ProjectResourcesBuildProcessor.cs
+++ Assets/UnityCommon/Editor/ProjectResourcesBuildProcessor.cs
@@ -6,23 +6,24 @@ namespace UnityCommon
 {
     public class ProjectResourcesBuildProcessor : IPreprocessBuildWithReport, IPostprocessBuildWithReport
     {
+        public static string TempFolderPath = "Assets/TEMP_UNITY_COMMON/Resources";
+
         public int callbackOrder => 100;
 
-        private const string tempFolderPath = "Assets/TEMP_UNITY_COMMON/Resources";
-        private const string assetPath = tempFolderPath + "/" + nameof(ProjectResources) + ".asset";
+        private static string assetPath => TempFolderPath + "/" + nameof(ProjectResources) + ".asset";
 
         public void OnPreprocessBuild (UnityEditor.Build.Reporting.BuildReport report)
         {
             var asset = ScriptableObject.CreateInstance<ProjectResources>();
             asset.LocateAllResources();
-            EditorUtils.CreateFolderAsset(tempFolderPath);
+            EditorUtils.CreateFolderAsset(TempFolderPath);
             AssetDatabase.CreateAsset(asset, assetPath);
             AssetDatabase.SaveAssets();
         }
 
         public void OnPostprocessBuild (UnityEditor.Build.Reporting.BuildReport report)
         {
-            AssetDatabase.DeleteAsset(tempFolderPath.GetBeforeLast("/"));
+            AssetDatabase.DeleteAsset(TempFolderPath.GetBeforeLast("/"));
             AssetDatabase.SaveAssets();
         }
     }
