bf69eef4b049bfd98cb663eb9170e5a0edf44af6
Artyom Sovetnikov <elringus@yandex.ru>
2020-02-01 12:51:26 +0300

    publish

--- Runtime/Utilities/AsyncUtils.cs
+++ Runtime/Utilities/AsyncUtils.cs
@@ -7,5 +7,7 @@ namespace UnityCommon
         public static YieldAwaitable WaitEndOfFrame => UniTask.Yield();
 
         public static UniTask.Awaiter GetAwaiter (this UniTask? task) => task.HasValue ? task.GetAwaiter() : UniTask.CompletedTask.GetAwaiter();
+
+        public static UniTask<T>.Awaiter GetAwaiter<T> (this UniTask<T>? task) => task.HasValue ? task.GetAwaiter() : UniTask.FromResult<T>(default).GetAwaiter();
     }
 }
