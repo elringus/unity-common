5a7247c372a398c9f2f30cbd90a5d64428aa5d08
Artyom Sovetnikov <elringus@yandex.ru>
2020-04-09 13:49:12 +0300

    add root path for project provider

--- Assets/UnityCommon/Runtime/ResourceProvider/ProjectFolderLocator.cs
+++ Assets/UnityCommon/Runtime/ResourceProvider/ProjectFolderLocator.cs
@@ -6,23 +6,27 @@ namespace UnityCommon
 {
     public class ProjectFolderLocator : LocateFoldersRunner
     {
+        public readonly string RootPath;
+
         private readonly ProjectResources projectResources;
 
-        public ProjectFolderLocator (IResourceProvider provider, string resourcesPath, ProjectResources projectResources)
+        public ProjectFolderLocator (IResourceProvider provider, string rootPath, string resourcesPath, ProjectResources projectResources)
             : base (provider, resourcesPath ?? string.Empty)
         {
+            RootPath = rootPath;
             this.projectResources = projectResources;
         }
 
         public override UniTask RunAsync ()
         {
-            var locatedFolders = LocateProjectFolders(Path, projectResources);
+            var locatedFolders = LocateProjectFolders(RootPath, Path, projectResources);
             SetResult(locatedFolders);
             return UniTask.CompletedTask;
         }
 
-        public static List<Folder> LocateProjectFolders (string path, ProjectResources projectResources)
+        public static List<Folder> LocateProjectFolders (string rootPath, string resourcesPath, ProjectResources projectResources)
         {
+            var path = string.IsNullOrEmpty(rootPath) ? resourcesPath : string.IsNullOrEmpty(resourcesPath) ? rootPath : $"{rootPath}/{resourcesPath}";
             return projectResources.ResourcePaths.LocateFolderPathsAtFolder(path).Select(p => new Folder(p)).ToList();
         }
     }
