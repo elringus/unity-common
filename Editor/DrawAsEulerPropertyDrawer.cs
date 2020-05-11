6cbb73be7d07fd4b6fb73bd38e7824511b09d9cd
Artyom Sovetnikov <elringus@yandex.ru>
2020-05-11 15:35:41 +0300

    publish

---
+++ Editor/DrawAsEulerPropertyDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityCommon
+{
+    [CustomPropertyDrawer(typeof(DrawAsEulerAttribute))]
+    public class DrawAsEulerPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI (Rect rect, SerializedProperty property, GUIContent label)
+        {
+            var euler = property.quaternionValue.eulerAngles;
+            EditorGUI.BeginProperty(rect, label, property);
+            EditorGUI.BeginChangeCheck();
+            euler = EditorGUI.Vector3Field(rect, label, euler);
+            if (EditorGUI.EndChangeCheck())
+                property.quaternionValue = Quaternion.Euler(euler);
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+}
---
+++ Runtime/Attributes/DrawAsEulerAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UnityCommon
+{
+    /// <summary>
+    /// When applied to a <see cref="Quaternion"/> field, draws it as <see cref="Vector3"/> euler.
+    /// </summary>
+    public class DrawAsEulerAttribute : PropertyAttribute { }
+}
