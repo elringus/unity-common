7d901d666e32ba17f96a7ac7d733a2f1036e0909
Artyom Sovetnikov <elringus@yandex.ru>
2020-07-23 15:29:56 +0300

    publish

--- Runtime/CancellationToken.cs
+++ Runtime/CancellationToken.cs
@@ -10,36 +10,42 @@ namespace UnityCommon
     public readonly struct CancellationToken : IEquatable<CancellationToken>
     {
         /// <summary>
+        /// Source token for ASAP cancellation scenario.
+        /// </summary>
+        public readonly System.Threading.CancellationToken ASAPToken;
+        /// <summary>
+        /// Source token for lazy cancellation scenario.
+        /// </summary>
+        public readonly System.Threading.CancellationToken LazyToken;
+        /// <summary>
         /// Whether cancellation of the async routine is requested (either ASAP or lazy).
         /// </summary>
-        public bool CancellationRequested => asapToken.IsCancellationRequested || lazyToken.IsCancellationRequested;
+        public bool CancellationRequested => ASAPToken.IsCancellationRequested || LazyToken.IsCancellationRequested;
         /// <inheritdoc cref="CancellationRequested"/>
         public bool IsCancellationRequested => CancellationRequested;
         /// <summary>
         /// Whether cancellation of the async routine is requested as soon as possible.
         /// </summary>
-        public bool CancelASAP => asapToken.IsCancellationRequested;
+        public bool CancelASAP => ASAPToken.IsCancellationRequested;
         /// <summary>
         /// Whether cancellation of the async routine is requested, but it's allowed to perform completion procedures beforehand.
         /// </summary>
-        public bool CancelLazy => !CancelASAP && lazyToken.IsCancellationRequested;
-
-        private readonly System.Threading.CancellationToken asapToken, lazyToken;
+        public bool CancelLazy => !CancelASAP && LazyToken.IsCancellationRequested;
 
         /// <param name="cancellationToken">Source cancellation token.</param>
         /// <param name="lazy">Whether the provided token allows lazy cancellation.</param>
         public CancellationToken (System.Threading.CancellationToken cancellationToken, bool lazy = false)
         {
-            if (lazy) lazyToken = cancellationToken;
-            else asapToken = cancellationToken;
+            if (lazy) LazyToken = cancellationToken;
+            else ASAPToken = cancellationToken;
         }
 
         /// <param name="asapToken">Source token for ASAP cancellation scenario.</param>
         /// <param name="lazyToken">Source token for lazy cancellation scenario.</param>
         public CancellationToken (System.Threading.CancellationToken asapToken, System.Threading.CancellationToken lazyToken)
         {
-            this.asapToken = asapToken;
-            this.lazyToken = lazyToken;
+            ASAPToken = asapToken;
+            LazyToken = lazyToken;
         }
 
         /// <summary>
@@ -48,7 +54,7 @@ namespace UnityCommon
         /// </summary>
         public System.Threading.CancellationTokenSource CreateLinkedTokenSource ()
         {
-            return System.Threading.CancellationTokenSource.CreateLinkedTokenSource(asapToken, lazyToken);
+            return System.Threading.CancellationTokenSource.CreateLinkedTokenSource(ASAPToken, LazyToken);
         }
 
         public static bool operator == (CancellationToken left, CancellationToken right) => left.Equals(right);
@@ -62,15 +68,15 @@ namespace UnityCommon
 
         public bool Equals (CancellationToken other)
         {
-            return EqualityComparer<System.Threading.CancellationToken>.Default.Equals(asapToken, other.asapToken) &&
-                   EqualityComparer<System.Threading.CancellationToken>.Default.Equals(lazyToken, other.lazyToken);
+            return EqualityComparer<System.Threading.CancellationToken>.Default.Equals(ASAPToken, other.ASAPToken) &&
+                   EqualityComparer<System.Threading.CancellationToken>.Default.Equals(LazyToken, other.LazyToken);
         }
 
         public override int GetHashCode ()
         {
             int hashCode = -1518707389;
-            hashCode = hashCode * -1521134295 + asapToken.GetHashCode();
-            hashCode = hashCode * -1521134295 + lazyToken.GetHashCode();
+            hashCode = hashCode * -1521134295 + ASAPToken.GetHashCode();
+            hashCode = hashCode * -1521134295 + LazyToken.GetHashCode();
             return hashCode;
         }
     }
