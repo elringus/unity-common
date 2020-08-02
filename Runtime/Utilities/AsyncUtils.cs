7cc7257cca84a69a8617d9776069d6db672d14a4
Artyom Sovetnikov <elringus@yandex.ru>
2020-08-02 16:20:08 +0300

    publish

--- Runtime/Utilities/AsyncUtils.cs
+++ Runtime/Utilities/AsyncUtils.cs
@@ -13,6 +13,9 @@ namespace UnityCommon
         #if !UNITASK2_AVAILABLE // For compatibility with UniTask v2 (it doesn't have UniTask.IsCompleted)
         public static bool IsCompleted (this UniTask task) => task.IsCompleted;
         public static bool IsCompleted<T> (this UniTask<T> task) => task.IsCompleted;
+        #else
+        public static bool IsCompleted (this UniTask task) => task.Status.IsCompleted();
+        public static bool IsCompleted<T> (this UniTask<T> task) => task.Status.IsCompleted();
         #endif
     }
 }
