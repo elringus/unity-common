295a712379538de5f03b9ea1318a250bd0c33a54
Artyom Sovetnikov <elringus@yandex.ru>
2020-08-02 16:11:17 +0300

    publish

--- Runtime/Utilities/AsyncUtils.cs
+++ Runtime/Utilities/AsyncUtils.cs
@@ -9,5 +9,10 @@ namespace UnityCommon
         public static UniTask.Awaiter GetAwaiter (this UniTask? task) => task.HasValue ? task.Value.GetAwaiter() : UniTask.CompletedTask.GetAwaiter();
 
         public static UniTask<T>.Awaiter GetAwaiter<T> (this UniTask<T>? task) => task.HasValue ? task.Value.GetAwaiter() : UniTask.FromResult<T>(default).GetAwaiter();
+
+        #if !UNITASK2_AVAILABLE // For compatibility with UniTask v2 (it doesn't have UniTask.IsCompleted)
+        public static bool IsCompleted (this UniTask task) => task.IsCompleted;
+        public static bool IsCompleted<T> (this UniTask<T> task) => task.IsCompleted;
+        #endif
     }
 }
