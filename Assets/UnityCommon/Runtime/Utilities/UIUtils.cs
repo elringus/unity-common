8e6a659847761669891e5f81befffb9d92181e51
Artyom Sovetnikov <elringus@yandex.ru>
2020-09-07 16:48:06 +0300

    add uiutils

---
+++ Assets/UnityCommon/Runtime/Utilities/UIUtils.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityCommon
+{
+    public static class UIUtils
+    {
+        public static void ScrollTo (this ScrollRect scroller, RectTransform child)
+        {
+            var contentPos = (Vector2)scroller.transform.InverseTransformPoint(scroller.content.position);
+            var childPos = (Vector2)scroller.transform.InverseTransformPoint(child.position);
+            var endPos = contentPos - childPos - child.sizeDelta / 2f;
+            if (!scroller.horizontal) endPos.x = scroller.content.anchoredPosition.x;
+            if (!scroller.vertical) endPos.y = scroller.content.anchoredPosition.y;
+            scroller.content.anchoredPosition = endPos;
+        }
+    }
+}
