e3852872a5a92e3eaa7be2f70af4102f7fcd769e
Artyom Sovetnikov <elringus@gmail.com>
2020-11-30 03:18:17 +0300

    publish

--- Runtime/Collections/IntRange.cs
+++ Runtime/Collections/IntRange.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UnityCommon
 {
@@ -6,8 +7,8 @@ namespace UnityCommon
     /// Represents an integer range starting with <see cref="StartIndex"/> and ending with <see cref="EndIndex"/>.
     /// Both endpoints are considered to be included.
     /// </summary>
-    [System.Serializable]
-    public struct IntRange
+    [Serializable]
+    public struct IntRange : IEquatable<IntRange>
     {
         public int StartIndex => startIndex;
         public int EndIndex => endIndex;
@@ -24,6 +25,38 @@ namespace UnityCommon
         public bool Contains (int index)
         {
             return index >= StartIndex && index <= EndIndex;
+        }
+        
+        public bool Equals (IntRange other)
+        {
+            return startIndex == other.startIndex && endIndex == other.endIndex;
+        }
+
+        public override bool Equals (object obj)
+        {
+            return obj is IntRange other && Equals(other);
+        }
+
+        public override int GetHashCode ()
+        {
+            unchecked
+            {
+                
+                // ReSharper disable NonReadonlyMemberInGetHashCode
+                // It's actually read-only (at runtime, that is).
+                return (startIndex * 397) ^ endIndex;
+                // ReSharper restore NonReadonlyMemberInGetHashCode
+            }
+        }
+
+        public static bool operator == (IntRange left, IntRange right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator != (IntRange left, IntRange right)
+        {
+            return !left.Equals(right);
         }
     }
 }
--- Runtime/Tween/FloatTween.cs
+++ Runtime/Tween/FloatTween.cs
@@ -2,7 +2,7 @@
 
 namespace UnityCommon
 {
-    public readonly struct FloatTween : ITweenValue
+    public readonly struct FloatTween : ITweenValue, IEquatable<FloatTween>
     {
         public float TweenDuration { get; }
         public EasingType EasingType { get; }
@@ -36,6 +36,51 @@ namespace UnityCommon
 
             var newValue = easingFunction(startValue, targetValue, tweenPercent);
             onTween.Invoke(newValue);
+        }
+        
+        public bool Equals (FloatTween other)
+        {
+            return startValue.Equals(other.startValue) && 
+                   targetValue.Equals(other.targetValue) && 
+                   Equals(onTween, other.onTween) && 
+                   Equals(easingFunction, other.easingFunction) && 
+                   Equals(target, other.target) && 
+                   targetProvided == other.targetProvided && 
+                   TweenDuration.Equals(other.TweenDuration) && 
+                   EasingType == other.EasingType && 
+                   TimeScaleIgnored == other.TimeScaleIgnored;
+        }
+
+        public override bool Equals (object obj)
+        {
+            return obj is FloatTween other && Equals(other);
+        }
+
+        public override int GetHashCode ()
+        {
+            unchecked
+            {
+                var hashCode = startValue.GetHashCode();
+                hashCode = (hashCode * 397) ^ targetValue.GetHashCode();
+                hashCode = (hashCode * 397) ^ (onTween != null ? onTween.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (easingFunction != null ? easingFunction.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (target != null ? target.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ targetProvided.GetHashCode();
+                hashCode = (hashCode * 397) ^ TweenDuration.GetHashCode();
+                hashCode = (hashCode * 397) ^ (int)EasingType;
+                hashCode = (hashCode * 397) ^ TimeScaleIgnored.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator == (FloatTween left, FloatTween right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator != (FloatTween left, FloatTween right)
+        {
+            return !left.Equals(right);
         }
     }
 }
