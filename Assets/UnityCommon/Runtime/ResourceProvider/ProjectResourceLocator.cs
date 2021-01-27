f8051bc0c0003ddb5d2b054a72a869b1cb799ed8
Artyom Sovetnikov <elringus@gmail.com>
2021-01-27 15:06:50 +0300

    fix project locator

--- Assets/UnityCommon/Runtime/ResourceProvider/ProjectResourceLocator.cs
+++ Assets/UnityCommon/Runtime/ResourceProvider/ProjectResourceLocator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UniRx.Async;
@@ -7,31 +8,24 @@ namespace UnityCommon
     public class ProjectResourceLocator<TResource> : LocateResourcesRunner<TResource>
         where TResource : UnityEngine.Object
     {
-        public readonly string RootPath;
+        private readonly IReadOnlyDictionary<string, Type> projectResources;
 
-        private readonly ProjectResources projectResources;
-
-        public ProjectResourceLocator (IResourceProvider provider, string rootPath, string resourcesPath,
-            ProjectResources projectResources) : base(provider, resourcesPath ?? string.Empty)
+        public ProjectResourceLocator (IResourceProvider provider, string resourcesPath,
+            IReadOnlyDictionary<string, Type> projectResources) : base(provider, resourcesPath ?? string.Empty)
         {
-            RootPath = rootPath;
             this.projectResources = projectResources;
         }
 
         public override UniTask RunAsync ()
         {
-            var locatedResourcePaths = LocateProjectResources(RootPath, Path, projectResources);
+            var locatedResourcePaths = LocateProjectResources(Path, projectResources);
             SetResult(locatedResourcePaths);
             return UniTask.CompletedTask;
         }
 
-        public static IReadOnlyCollection<string> LocateProjectResources (string rootPath, string resourcesPath, ProjectResources projectResources)
+        public static IReadOnlyCollection<string> LocateProjectResources (string resourcesPath, IReadOnlyDictionary<string, Type> projectResources)
         {
-            var path = string.IsNullOrEmpty(rootPath) ? resourcesPath : string.IsNullOrEmpty(resourcesPath) ? rootPath : $"{rootPath}/{resourcesPath}";
-            var result = projectResources.Resources.Keys.LocateResourcePathsAtFolder(path);
-            if (!string.IsNullOrEmpty(rootPath))
-                return result.Select(p => p.GetAfterFirst(rootPath + "/")).ToArray();
-            return result.ToArray();
+            return projectResources.Keys.LocateResourcePathsAtFolder(resourcesPath).ToArray();
         }
     }
 }
