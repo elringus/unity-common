ee528d43b5641c505e7e5a38e0c3679637525ae6
Artyom Sovetnikov <elringus@gmail.com>
2021-02-11 19:30:09 +0300

    optimize locate all

--- Assets/UnityCommon/Runtime/ResourceProvider/ProjectResources.cs
+++ Assets/UnityCommon/Runtime/ResourceProvider/ProjectResources.cs
@@ -10,11 +10,14 @@ namespace UnityCommon
     {
         #pragma warning disable 0649
         [Serializable]
-        private struct ProjectResource { public string Path, Type; }
+        private struct ProjectResource
+        {
+            public string Path, Type;
+        }
         #pragma warning restore 0649
 
         public const string ResourcePath = "UnityCommon/ProjectResources";
-        
+
         [SerializeField] private List<ProjectResource> resourcePaths = new List<ProjectResource>();
 
         public static ProjectResources Get ()
@@ -45,43 +48,24 @@ namespace UnityCommon
             }
         }
 
-        private static void WalkResourcesDirectory (DirectoryInfo directory, List<ProjectResource> outPaths)
-        {
-            var paths = directory.GetFiles().Where(IsNotMetaFile).Select(GetAssetPath);
-            foreach (var path in paths)
-                AddPathUsingEditorAPI(path);
-
-            var subDirs = directory.GetDirectories();
-            foreach (var dirInfo in subDirs)
-                WalkResourcesDirectory(dirInfo, outPaths);
-
-            bool IsNotMetaFile (FileInfo info) => !info.FullName.EndsWithFast(".meta");
-            
-            string GetAssetPath (FileInfo info) => PathUtils.AbsoluteToAssetPath(info.FullName);
-
-            void AddPathUsingEditorAPI (string assetPath)
-            {
-                #if UNITY_EDITOR
-                var type = UnityEditor.AssetDatabase.GetMainAssetTypeAtPath(assetPath);
-                if (type is null) throw new Exception($"Failed to get type of `{assetPath}` asset.");
-                outPaths.Add(new ProjectResource { Path = GetResourcePath(), Type = type.AssemblyQualifiedName });
-                string GetResourcePath ()
-                {
-                    assetPath = assetPath.GetAfterFirst("/Resources/");
-                    return assetPath.Contains(".") ? assetPath.GetBeforeLast(".") : assetPath;
-                }
-                #endif
-            }
-        }
-
         private void LocateAllResources ()
         {
+            #if UNITY_EDITOR
             resourcePaths.Clear();
-            var dataDir = new DirectoryInfo(Application.dataPath);
-            var resourcesDirs = dataDir.GetDirectories("*Resources", SearchOption.AllDirectories)
-                .Where(d => d.FullName.EndsWithFast($"{Path.DirectorySeparatorChar}Resources")).ToList();
-            foreach (var dir in resourcesDirs)
-                WalkResourcesDirectory(dir, resourcePaths);
+            foreach (var path in UnityEditor.AssetDatabase.GetAllAssetPaths())
+            {
+                if (!path.Contains("/Resources/")) continue;
+                var type = UnityEditor.AssetDatabase.GetMainAssetTypeAtPath(path);
+                if (type is null) continue;
+                resourcePaths.Add(new ProjectResource { Path = GetPath(path), Type = type.AssemblyQualifiedName });
+            }
+
+            string GetPath (string path)
+            {
+                path = path.GetAfterFirst("/Resources/");
+                return path.Contains(".") ? path.GetBeforeLast(".") : path;
+            }
+            #endif
         }
     }
 }
