cad7ec244ee045b45f618d9bbe97eb8c2c7db192
Artyom Sovetnikov <elringus@gmail.com>
2021-02-19 23:11:46 +0300

    refactor grids

--- Assets/UnityCommon/Runtime/ScriptableUI/ScriptableGridSlot.cs
+++ Assets/UnityCommon/Runtime/ScriptableUI/ScriptableGridSlot.cs
@@ -1,52 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace UnityCommon
 {
     [RequireComponent(typeof(CanvasGroup)), RequireComponent(typeof(UnityEngine.UI.Button))]
     public class ScriptableGridSlot : ScriptableButton, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
-    { 
-        public class Constructor<TSlot> where TSlot : ScriptableGridSlot
-        {
-            public readonly TSlot ConstructedSlot;
+    {
+        [Serializable]
+        private class OnClickedEvent : UnityEvent<string> { }
 
-            public Constructor (TSlot prototype, string id, OnClicked onClicked = null)
-            {
-                ConstructedSlot = Instantiate(prototype);
-                ConstructedSlot.Id = id;
-                ConstructedSlot.onClickedAction = onClicked;
-            }
-        }
-
-        /// <summary>
-        /// Action to invoke when the slot body is clicked by the user.
-        /// </summary>
-        /// <param name="slotId">ID of the clicked slot.</param>
-        public delegate void OnClicked (string slotId);
-
-        public virtual string Id { get; private set; }
-        public virtual int NumberInGrid => transform.GetSiblingIndex() + 1;
+        public virtual string Id { get; set; }
 
         [Tooltip("Opacity to fade to when the slot is hovered or selected; set to zero to disable the fade behaviour.")]
         [SerializeField] private float hoverOpacityFade = .25f;
+        [SerializeField] private OnClickedEvent onClicked = default;
 
-        private Tweener<FloatTween> fadeTweener;
-        private OnClicked onClickedAction;
-
-        protected override void Awake ()
-        {
-            base.Awake();
-
-            fadeTweener = new Tweener<FloatTween>();
-        }
-
-        protected override void Start ()
-        {
-            base.Start();
-
-            if (hoverOpacityFade > 0)
-                SetOpacity(1 - hoverOpacityFade);
-        }
+        private readonly Tweener<FloatTween> fadeTweener = new Tweener<FloatTween>();
 
         public virtual void OnPointerEnter (PointerEventData eventData) => FadeInSlot();
 
@@ -56,11 +27,19 @@ namespace UnityCommon
 
         public virtual void OnDeselect (BaseEventData eventData) => FadeOutSlot();
 
+        protected override void Start ()
+        {
+            base.Start();
+
+            if (hoverOpacityFade > 0)
+                SetOpacity(1 - hoverOpacityFade);
+        }
+
         protected override void OnButtonClick ()
         {
             base.OnButtonClick();
 
-            onClickedAction?.Invoke(Id);
+            onClicked?.Invoke(Id);
         }
 
         protected virtual void FadeInSlot ()
