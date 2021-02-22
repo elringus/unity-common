dff2653b6878be16f837db5678fa3e981bccf785
Artyom Sovetnikov <elringus@gmail.com>
2021-02-22 15:40:52 +0300

    publish

--- Runtime/ResourceProvider/ResourceLoaderExtensions.cs
+++ Runtime/ResourceProvider/ResourceLoaderExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UniRx.Async;
 
 namespace UnityCommon
@@ -17,7 +18,7 @@ namespace UnityCommon
             if (!string.IsNullOrEmpty(localPath))
                 loader.Hold(localPath, holder);
         }
-        
+
         /// <summary>
         /// Given provided resource is loaded by the loader, release it.
         /// </summary>
@@ -27,11 +28,11 @@ namespace UnityCommon
             if (!string.IsNullOrEmpty(localPath))
                 loader.Release(localPath, holder, unload);
         }
-        
+
         /// <summary>
         /// Attempts to load a resource with the provided path and holds it in case it's loaded successfully.
         /// </summary>
-        public static async UniTask<Resource<TResource>> LoadAndHoldAsync<TResource> (this IResourceLoader<TResource> loader, string path, object holder)
+        public static async UniTask<TResource> LoadAndHoldAsync<TResource> (this IResourceLoader<TResource> loader, string path, object holder)
             where TResource : UnityEngine.Object
         {
             var resource = await loader.LoadAsync(path);
@@ -43,7 +44,7 @@ namespace UnityCommon
         /// <summary>
         /// Attempts to load all the available resources (optionally) filtered by a base path and holds each of them.
         /// </summary>
-        public static async UniTask<IReadOnlyCollection<Resource>> LoadAndHoldAllAsync<TResource> (this IResourceLoader<TResource> loader, object holder, string path = null)
+        public static async UniTask<IReadOnlyCollection<TResource>> LoadAndHoldAllAsync<TResource> (this IResourceLoader<TResource> loader, object holder, string path = null)
             where TResource : UnityEngine.Object
         {
             var resources = await loader.LoadAllAsync(path);
@@ -53,7 +54,7 @@ namespace UnityCommon
                 if (!string.IsNullOrEmpty(localPath))
                     loader.Hold(localPath, holder);
             }
-            return resources;
+            return resources.Select(r => r.Object).ToArray();
         }
     }
 }
