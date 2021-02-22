b81c9bc014a176a5f7126d1daea1e7d48ab7d6f9
Artyom Sovetnikov <elringus@gmail.com>
2021-02-22 13:53:45 +0300

    publish

--- Runtime/ResourceProvider/IHoldersTracker.cs
+++ Runtime/ResourceProvider/IHoldersTracker.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace UnityCommon
 {
@@ -7,8 +7,24 @@ namespace UnityCommon
     /// </summary>
     public interface IHoldersTracker
     {
+        /// <summary>
+        /// Adds provided object as a holder of the Unity object.
+        /// The Unity object won't be unloaded (destroyed), as long at it has at least one holder.
+        /// </summary>
+        /// <param name="obj">The Unity object to hold.</param>
+        /// <param name="holder">Holder of the Unity object.</param>
+        /// <returns>Total number of unique holders of the Unity object after the addition.</returns>
         int Hold (Object obj, object holder);
+        /// <summary>
+        /// Removes provided object from holders of the Unity object.
+        /// </summary>
+        /// <param name="obj">The Unity object which should no longer be held by the object.</param>
+        /// <param name="holder">The holder object to remove.</param>
+        /// <returns>Total number of unique holders of the Unity object after the removal.</returns>
         int Release (Object obj, object holder);
+        /// <summary>
+        /// Returns total number of unique holders of the Unity object.
+        /// </summary>
         int CountHolders (Object obj);
     }
 }
