b338b3beb6774b677a05b0b3597b5527bfe87732
Artyom Sovetnikov <elringus@gmail.com>
2021-04-30 00:04:10 +0300

    remove uni task completed

--- Assets/UnityCommon/Runtime/Utilities/AsyncUtils.cs
+++ Assets/UnityCommon/Runtime/Utilities/AsyncUtils.cs
@@ -9,13 +9,5 @@ namespace UnityCommon
         public static UniTask.Awaiter GetAwaiter (this UniTask? task) => task?.GetAwaiter() ?? UniTask.CompletedTask.GetAwaiter();
 
         public static UniTask<T>.Awaiter GetAwaiter<T> (this UniTask<T>? task) => task?.GetAwaiter() ?? UniTask.FromResult<T>(default).GetAwaiter();
-
-        #if !UNITASK2_AVAILABLE // For compatibility with UniTask v2 (it doesn't have UniTask.IsCompleted)
-        public static bool IsCompleted (this UniTask task) => task.IsCompleted;
-        public static bool IsCompleted<T> (this UniTask<T> task) => task.IsCompleted;
-        #else
-        public static bool IsCompleted (this UniTask task) => task.Status.IsCompleted();
-        public static bool IsCompleted<T> (this UniTask<T> task) => task.Status.IsCompleted();
-        #endif
     }
 }
