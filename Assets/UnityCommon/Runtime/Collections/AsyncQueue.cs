18f85459007e8d1fdecd02d38c3dee71146bd57d
Artyom Sovetnikov <elringus@gmail.com>
2021-09-20 17:13:35 +0300

    add async queue

---
+++ Assets/UnityCommon/Runtime/Collections/AsyncQueue.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using UniRx.Async;
+
+namespace UnityCommon
+{
+    public class AsyncQueue<T> : IReadOnlyCollection<T>, IDisposable
+    {
+        public int Count => queue.Count;
+
+        private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(0);
+
+        public void Enqueue (T item)
+        {
+            queue.Enqueue(item);
+            semaphore.Release();
+        }
+
+        public async UniTask<T> WaitAsync (AsyncToken token)
+        {
+            while (!token.Canceled)
+            {
+                await semaphore.WaitAsync(token.CancellationToken);
+                if (queue.TryDequeue(out var message)) return message;
+            }
+            throw new OperationCanceledException();
+        }
+
+        public void Dispose () => semaphore.Dispose();
+        public IEnumerator<T> GetEnumerator () => queue.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator () => GetEnumerator();
+    }
+}
