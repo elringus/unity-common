2b172ad6f616493591a8288e04b29d13c62aa722
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-06-01 16:49:40 +0300

    publish

---
+++ Runtime/Async/CancelOnDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using UnityEngine;
+
+namespace UnityCommon
+{
+    public class CancelOnDestroy : MonoBehaviour
+    {
+        public CancellationToken Token => cts.Token;
+
+        private readonly CancellationTokenSource cts = new CancellationTokenSource();
+
+        private void OnDestroy ()
+        {
+            cts.Cancel();
+            cts.Dispose();
+        }
+    }
+}
