3474970c62ecdee242e32889fc39f29b77e0877a
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-04-16 19:55:01 +0300

    publish

--- Runtime/ResourceProvider/ResourceLoaderExtensions.cs
+++ Runtime/ResourceProvider/ResourceLoaderExtensions.cs
@@ -16,7 +16,7 @@ namespace UnityCommon
             if (!string.IsNullOrEmpty(localPath))
                 loader.Hold(localPath, holder);
         }
-        
+
         /// <summary>
         /// Given provided resource is loaded by the loader, release it.
         /// </summary>
@@ -26,12 +26,11 @@ namespace UnityCommon
             if (!string.IsNullOrEmpty(localPath))
                 loader.Release(localPath, holder, unload);
         }
-        
+
         /// <summary>
         /// Attempts to load a resource with the provided path and holds it in case it's loaded successfully.
         /// </summary>
-        public static async UniTask<Resource<TResource>> LoadAndHoldAsync<TResource> (this IResourceLoader<TResource> loader, string path, object holder)
-            where TResource : UnityEngine.Object
+        public static async UniTask<Resource> LoadAndHoldAsync (this IResourceLoader loader, string path, object holder)
         {
             var resource = await loader.LoadAsync(path);
             if (loader.IsLoaded(path))
@@ -42,6 +41,29 @@ namespace UnityCommon
         /// <summary>
         /// Attempts to load all the available resources (optionally) filtered by a base path and holds each of them.
         /// </summary>
+        public static async UniTask<IReadOnlyCollection<Resource>> LoadAndHoldAllAsync (this IResourceLoader loader, object holder, string path = null)
+        {
+            var resources = await loader.LoadAllAsync(path);
+            foreach (var resource in resources)
+            {
+                var localPath = loader.GetLocalPath(resource);
+                if (!string.IsNullOrEmpty(localPath))
+                    loader.Hold(localPath, holder);
+            }
+            return resources;
+        }
+
+        /// <inheritdoc cref="LoadAndHoldAsync"/>
+        public static async UniTask<Resource<TResource>> LoadAndHoldAsync<TResource> (this IResourceLoader<TResource> loader, string path, object holder)
+            where TResource : UnityEngine.Object
+        {
+            var resource = await loader.LoadAsync(path);
+            if (loader.IsLoaded(path))
+                loader.Hold(path, holder);
+            return resource;
+        }
+
+        /// <inheritdoc cref="LoadAndHoldAllAsync"/>
         public static async UniTask<IReadOnlyCollection<Resource<TResource>>> LoadAndHoldAllAsync<TResource> (this IResourceLoader<TResource> loader, object holder, string path = null)
             where TResource : UnityEngine.Object
         {
