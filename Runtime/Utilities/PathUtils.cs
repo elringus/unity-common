b9d82e176ed00872be365f3691c93d9444e1e90f
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-08 18:33:27 +0300

    publish

--- Runtime/Utilities/PathUtils.cs
+++ Runtime/Utilities/PathUtils.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
@@ -6,12 +7,30 @@ namespace UnityCommon
     public static class PathUtils
     {
         /// <summary>
+        /// Replaces back slashes with forward slashes.
+        /// </summary>
+        public static string FormatPath (string path)
+        {
+            return path.Replace('\\', '/');
+        }
+
+        /// <summary>
+        /// Modifies the collection replacing back slashes with forward slashes.
+        /// </summary>
+        public static T FormatPaths<T> (T paths) where T : IList<string>
+        {
+            for (int i = 0; i < paths.Count; i++)
+                paths[i] = FormatPath(paths[i]);
+            return paths;
+        }
+
+        /// <summary>
         /// Given an absolute path inside current Unity project (eg, `C:\UnityProject\Assets\FooAsset.asset`),
         /// transforms it to a relative project asset path (eg, `Assets/FooAsset.asset`).
         /// </summary>
         public static string AbsoluteToAssetPath (string absolutePath)
         {
-            absolutePath = absolutePath.Replace("\\", "/");
+            absolutePath = FormatPath(absolutePath);
             if (!absolutePath.StartsWithFast(Application.dataPath)) return null;
             return "Assets" + absolutePath.Replace(Application.dataPath, string.Empty);
         }
@@ -21,7 +40,7 @@ namespace UnityCommon
         /// </summary>
         public static string Combine (params string[] paths)
         {
-            return Path.Combine(paths).Replace("\\", "/");
+            return FormatPath(Path.Combine(paths));
         }
     }
 }
