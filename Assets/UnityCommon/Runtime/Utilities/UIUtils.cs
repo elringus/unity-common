eea622f78f3afc05070e326ced8c624cc57435e7
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-16 21:48:43 +0300

    add scrollbar contains

--- Assets/UnityCommon/Runtime/Utilities/UIUtils.cs
+++ Assets/UnityCommon/Runtime/Utilities/UIUtils.cs
@@ -5,6 +5,11 @@ namespace UnityCommon
 {
     public static class UIUtils
     {
+        /// <summary>
+        /// Changes scrollbar's scroll position to contain specified child item.
+        /// </summary>
+        /// <param name="scroller"></param>
+        /// <param name="child"></param>
         public static void ScrollTo (this ScrollRect scroller, RectTransform child)
         {
             var contentPos = (Vector2)scroller.transform.InverseTransformPoint(scroller.content.position);
@@ -13,6 +18,16 @@ namespace UnityCommon
             if (!scroller.horizontal) endPos.x = scroller.content.anchoredPosition.x;
             if (!scroller.vertical) endPos.y = scroller.content.anchoredPosition.y;
             scroller.content.anchoredPosition = endPos;
+        }
+
+        /// <summary>
+        /// Checks whether scrollbar contains both min and max points of the specified child item rect.
+        /// </summary>
+        public static bool Contains (this ScrollRect scroller, RectTransform child)
+        {
+            var min = scroller.viewport.InverseTransformPoint(child.TransformPoint(child.rect.min));
+            var max = scroller.viewport.InverseTransformPoint(child.TransformPoint(child.rect.max));
+            return scroller.viewport.rect.Contains(min) && scroller.viewport.rect.Contains(max);
         }
     }
 }
