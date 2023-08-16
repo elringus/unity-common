3d6fb6a569c77f0cb4d22c703d288d1bc209f3a1
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-17 00:26:47 +0300

    update ui utils

--- Assets/UnityCommon/Runtime/Utilities/UIUtils.cs
+++ Assets/UnityCommon/Runtime/Utilities/UIUtils.cs
@@ -8,25 +8,25 @@ namespace UnityCommon
         /// <summary>
         /// Changes scrollbar's scroll position to contain specified child item.
         /// </summary>
-        /// <param name="scroller"></param>
-        /// <param name="child"></param>
-        public static void ScrollTo (this ScrollRect scroller, RectTransform child)
+        public static void ScrollTo (this ScrollRect scroller, RectTransform item)
         {
-            var contentPos = (Vector2)scroller.transform.InverseTransformPoint(scroller.content.position);
-            var childPos = (Vector2)scroller.transform.InverseTransformPoint(child.position);
-            var endPos = contentPos - childPos - child.sizeDelta / 2f;
-            if (!scroller.horizontal) endPos.x = scroller.content.anchoredPosition.x;
-            if (!scroller.vertical) endPos.y = scroller.content.anchoredPosition.y;
-            scroller.content.anchoredPosition = endPos;
+            var viewPos = (Vector2)scroller.transform.InverseTransformPoint(scroller.content.position);
+            var itemPos = (Vector2)scroller.transform.InverseTransformPoint(item.position);
+            var toPos = viewPos - itemPos - item.sizeDelta / 2f;
+            if (!scroller.horizontal) toPos.x = scroller.content.anchoredPosition.x;
+            if (!scroller.vertical) toPos.y = scroller.content.anchoredPosition.y;
+            scroller.content.anchoredPosition = toPos;
         }
 
         /// <summary>
         /// Checks whether scrollbar contains both min and max points of the specified child item rect.
         /// </summary>
-        public static bool Contains (this ScrollRect scroller, RectTransform child)
+        public static bool Contains (this ScrollRect scroller, RectTransform item)
         {
-            var min = scroller.viewport.InverseTransformPoint(child.TransformPoint(child.rect.min));
-            var max = scroller.viewport.InverseTransformPoint(child.TransformPoint(child.rect.max));
+            var min = scroller.viewport.InverseTransformPoint(item.TransformPoint(item.rect.min));
+            var max = scroller.viewport.InverseTransformPoint(item.TransformPoint(item.rect.max));
+            if (!scroller.horizontal) min.x = max.x = scroller.viewport.rect.center.x;
+            if (!scroller.vertical) min.y = max.y = scroller.viewport.rect.center.y;
             return scroller.viewport.rect.Contains(min) && scroller.viewport.rect.Contains(max);
         }
     }
