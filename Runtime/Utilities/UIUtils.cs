64584e3f20ac555360b70f77e6754ed3669d41fa
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-28 17:50:44 +0300

    publish

--- Runtime/Utilities/UIUtils.cs
+++ Runtime/Utilities/UIUtils.cs
@@ -5,14 +5,29 @@ namespace UnityCommon
 {
     public static class UIUtils
     {
-        public static void ScrollTo (this ScrollRect scroller, RectTransform child)
+        /// <summary>
+        /// Changes scrollbar's scroll position to contain specified child item.
+        /// </summary>
+        public static void ScrollTo (this ScrollRect scroller, RectTransform item)
         {
-            var contentPos = (Vector2)scroller.transform.InverseTransformPoint(scroller.content.position);
-            var childPos = (Vector2)scroller.transform.InverseTransformPoint(child.position);
-            var endPos = contentPos - childPos - child.sizeDelta / 2f;
-            if (!scroller.horizontal) endPos.x = scroller.content.anchoredPosition.x;
-            if (!scroller.vertical) endPos.y = scroller.content.anchoredPosition.y;
-            scroller.content.anchoredPosition = endPos;
+            var viewPos = (Vector2)scroller.transform.InverseTransformPoint(scroller.content.position);
+            var itemPos = (Vector2)scroller.transform.InverseTransformPoint(item.position);
+            var toPos = viewPos - itemPos - item.sizeDelta / 2f;
+            if (!scroller.horizontal) toPos.x = scroller.content.anchoredPosition.x;
+            if (!scroller.vertical) toPos.y = scroller.content.anchoredPosition.y;
+            scroller.content.anchoredPosition = toPos;
+        }
+
+        /// <summary>
+        /// Checks whether scrollbar contains both min and max points of the specified child item rect.
+        /// </summary>
+        public static bool Contains (this ScrollRect scroller, RectTransform item)
+        {
+            var min = scroller.viewport.InverseTransformPoint(item.TransformPoint(item.rect.min));
+            var max = scroller.viewport.InverseTransformPoint(item.TransformPoint(item.rect.max));
+            if (!scroller.horizontal) min.x = max.x = scroller.viewport.rect.center.x;
+            if (!scroller.vertical) min.y = max.y = scroller.viewport.rect.center.y;
+            return scroller.viewport.rect.Contains(min) && scroller.viewport.rect.Contains(max);
         }
     }
 }
